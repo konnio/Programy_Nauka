8cbec8511a525439010780e5869927567aaf4ffa
konnio <konnio@gmail.com>
2017-11-24 10:51:19 +0100

    z dnia 24.11.2017

---
+++ ascii.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*  Deklaracja stalych
+	const int dni = 7;
+    	const float pi =  3.1459, e = 2/71828;
+	const double Avogardo = 6.022E23;
+
+
+*/
+
+int main (){
+	putchar('a');
+	putchar(97);
+	puts("\x61"); //wyswietla literę a wywolana zapisem 16-tkowym
+	puts("\n\'\"\n\t"); //put string wyswietla ciag znakow
+	return 0;
+}
+
---
+++ notatka_jezyk_c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*  Deklaracja stalych
+	const int dni = 7;
+    	const float pi =  3.1459, e = 2/71828;
+	const double Avogardo = 6.022E23;
+
+#	Notacja wegierska - przedrostek przed nazwa zmiennej bZmienna oznacza typ bolean
+
+#	Typy wyliczeniowe
+	enum
+	
+*/
+
+int main (){
+	putchar('a');
+	putchar(97);
+	puts("\x61"); //wyswietla literę a wywolana zapisem 16-tkowym
+	puts("\n\'\"\n\t"); //put string wyswietla ciag znakow
+	return 0;
+}
+
